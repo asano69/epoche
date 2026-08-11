e0c9f453ca7d696af2836a1afe56f01398546826
asano69 <186772504+asano69@users.noreply.github.com>
2026-08-11 09:56:19 +0900

    choer

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   const handleLogout = () => pb.authStore.clear();
 
   return (
-    <div class="mb-10 flex w-full flex-wrap items-center justify-between gap-y-3">
+    <div class="mb-10 flex w-full flex-wrap items-center justify-between ">
       <Logo linkable />
       <nav class="flex flex-wrap items-center gap-3">
         <button type="button" class="btn" onClick={handleLogout}>
